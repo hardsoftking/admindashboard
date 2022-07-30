943e7c0b5154e07355bb9e73afde227dd255e96c
wb505 <82988726+wb505@users.noreply.github.com>
2022-07-30 21:07:45 +0600

    Updating nav.js

--- assets/js/topNav.js
+++ assets/js/topNav.js
@@ -1,9 +1,19 @@
-
 /*=== 
 import removeLeftnav() from "./leftsideMenu.js"
 ===*/
 
   let mainContainer_src = document.body.querySelector("#main-container");
+/*==== flag ====*/
+ let flagIcon = document.body.querySelector(".flagIcon")
+ let flagBox = document.body.querySelector(".flagBox")
+ 
+/*==== profile ====*/
+ let profileIcon = document.body.querySelector(".user-profileIcon")
+ let profileBox = document.body.querySelector(".profileBox")
+
+/*==== settingIcon ====*/
+ let settingIcon = document.body.querySelector(".settingIcon")
+ let settingBox = document.body.querySelector(".setting-container")
 
 /*===== Notification =====*/
   let notiIcon = document.body.querySelector(".notificationIcon");
@@ -18,6 +28,10 @@ import removeLeftnav() from "./leftsideMenu.js"
       targetIcon.addEventListener("click",()=>{
         removeLeftnav()
         removeNotification()
+        removeFlagBox()
+        removeProfileBox()
+        removeSettingBox()
+        
         targetElem.classList.toggle(`${targetClass}`)
       })
     }
@@ -35,6 +49,10 @@ import removeLeftnav() from "./leftsideMenu.js"
      targetIcon.addEventListener("click",()=>{
         removeLeftnav()
         removeSearch()
+        removeFlagBox()
+        removeProfileBox()
+        removeSettingBox()
+        
         targetElem.classList.toggle(`${targetClass}`)
       })
     }
@@ -45,6 +63,69 @@ import removeLeftnav() from "./leftsideMenu.js"
     }
    /*close notiBox when click inside the main-container*/ 
     closeBox(notiBox,"activeNotiBox")
+    
+    
+   /*=====function for Toggle Flag =====*/
+    function handleFlagBox(targetIcon,targetElem,targetClass){
+     targetIcon.addEventListener("click",()=>{
+        removeLeftnav()
+        removeSearch()
+        removeProfileBox()
+        removeSettingBox()
+        removeNotification()
+        
+        targetElem.classList.toggle(`${targetClass}`)
+      })
+    }
+    handleFlagBox(flagIcon,flagBox,"activeFlagBox")
+    
+    function removeFlagBox(){
+      flagBox.classList.remove("activeFlagBox")
+    }
+   /*close notiBox when click inside the main-container*/ 
+    closeBox(flagBox,"activeFlagBox")
+    
+    
+   /*=====function for Toggle profileBox =====*/
+    function handleProfileBox(targetIcon,targetElem,targetClass){
+     targetIcon.addEventListener("click",()=>{
+        removeLeftnav()
+        removeSearch()
+        removeFlagBox()
+        removeNotification()
+        removeSettingBox()
+        
+        targetElem.classList.toggle(`${targetClass}`)
+      })
+    }
+    handleProfileBox(profileIcon,profileBox,"activeProfileBox")
+    
+    function removeProfileBox(){
+      profileBox.classList.remove("activeProfileBox")
+    }
+   /*close notiBox when click inside the main-container*/ 
+    closeBox(profileBox,"activeProfileBox")
+    
+    
+   /*=====function for Toggle Setting =====*/
+    function handleSettingBox(targetIcon,targetElem,targetClass){
+     targetIcon.addEventListener("click",()=>{
+        removeLeftnav()
+        removeSearch()
+        removeProfileBox()
+        removeFlagBox()
+        removeNotification()
+        
+        targetElem.classList.toggle(`${targetClass}`)
+      })
+    }
+    handleSettingBox(settingIcon,settingBox,"activeSettingBox")
+    
+    function removeSettingBox(){
+      settingBox.classList.remove("activeSettingBox")
+    }
+   /*close notiBox when click inside the main-container*/ 
+    closeBox(settingBox,"activeSettingBox")
     
     
     
