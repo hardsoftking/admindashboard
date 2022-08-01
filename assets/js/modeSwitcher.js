e2b2109614f363c7b9ed7458c08f6be41f7165f3
wb505 <82988726+wb505@users.noreply.github.com>
2022-08-02 01:24:36 +0600

    Delete modeSwitcher.js

--- assets/js/modeSwitcher.js
+++
@@ -1,8 +0,0 @@
-
-let body = document.body;
-let darkMode = document.body.querySelector(".darkMode")
-
-darkMode.addEventListener("click",()=>{
-  console.log("darmofe")
-  body.classList.toggle("activeDarkMode")
-})
